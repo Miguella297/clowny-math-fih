68620b296248c48b5e00111cc49440942eb19a10
Miguella297 <mikeivanov2010@gmail.com>
2026-03-18 23:26:13 -0400

    working on main menu
    yahoo

---
+++ main_menu.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("level1.tscn")
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("all levels.tscn")
+
+
+func _on_button_3_pressed() -> void:
+	get_tree().change_scene_to_file("how.tscn")
