bc0ea65a99e6d2ad4b1cbc00b511f2881b98cb90
Miguella297 <mikeivanov2010@gmail.com>
2026-03-19 00:44:40 -0400

    Finished all levels scene... For now.

--- all_levels.gd
+++ all_levels.gd
@@ -2,3 +2,6 @@ extends Node2D
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("level1.tscn")
+
+func _on_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("Main menu.tscn")
