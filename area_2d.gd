541dab38db66552f9462af36a65c1d369dad51ef
Miguella297 <mikeivanov2010@gmail.com>
2026-03-21 00:24:35 -0400

    deathscene made
    mmm

--- area_2d.gd
+++ area_2d.gd
@@ -1,4 +1,17 @@
 extends Area2D
 
 # Path to the new scene you want to load
-@export var target_scene_path : String: "res://"
+@export var target_scene_path: String = "res://deathscene.tscn"
+
+func _ready():
+	# Connect the  signal (if not already connected in the editor)
+	connect("body_entered", _on_body_entered)
+	
+func _on_body_entered(body):
+	#check if the body is in your player (assuming it's a KinematicBody2D or Characterbody2D)
+	
+	if body.is_in_group("Player"):
+		call_deferred("_fix")
+
+func _fix():
+	get_tree().change_scene_to_file(target_scene_path)
