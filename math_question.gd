083c521ff8759e4f4c3d587f72d89265b8fc4149
Miguella297 <mikeivanov2010@gmail.com>
2026-03-23 02:49:37 -0400

    LEVEL 1 DONE

--- math_question.gd
+++ math_question.gd
@@ -4,7 +4,7 @@ func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("deathscene.tscn")
 
 func _on_button_2_pressed() -> void:
-	get_tree().change_scene_to_file("level1.1.tscn")
+	get_tree().change_scene_to_file("level 1.1.tscn")
 
 func _on_button_3_pressed() -> void:
 	get_tree().change_scene_to_file("deathscene.tscn")
--- math_question_3.gd
+++ math_question_3.gd
@@ -4,7 +4,7 @@ func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("level1-2.tscn")
 
 func _on_button_2_pressed() -> void:
-	get_tree().change_scene_to_file("deathscene1.2")
+	get_tree().change_scene_to_file("deathscene1.2.tscn")
 
 func _on_button_3_pressed() -> void:
-	get_tree().change_scene_to_file("deathscene1.2")
+	get_tree().change_scene_to_file("deathscene1.2.tscn")
