81bb08b5f6fec7ec546b9aae45175021c80c6583
Miguella297 <mikeivanov2010@gmail.com>
2026-03-25 18:37:53 -0400

    enemies half way done

--- area_2d_2+2.gd
+++ area_2d_2+2.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 # Path to the new scene you want to load
-@export var target_scene_path: String = "res://deathscene2.tscn"
+@export var target_scene_path: String = "res://deathscene2.1.tscn"
 
 func _ready():
 	# Connect the signal (if not already connected in the editor)
---
+++ area_2d_2+6.gd
@@ -0,0 +1,4 @@
+extends Area2D
+
+# Path to the new scene you want to load
+@export var target_scene_path: String = "res://deathscene2.1.tscn"
