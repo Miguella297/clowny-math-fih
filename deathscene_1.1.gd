6802152eee88983f9a81f0098dc0c48405b9f556
Miguella297 <mikeivanov2010@gmail.com>
2026-03-26 21:50:10 -0400

    game main levels done and congrats page

--- deathscene_1.1.gd
+++ deathscene_1.1.gd
@@ -2,3 +2,7 @@ extends Node2D
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("level 1.1.tscn")
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("Main menu.tscn")
