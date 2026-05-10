3a6135a608695c7592b382cda5464f6c42905289
Miguella297 <mikeivanov2010@gmail.com>
2026-05-10 13:47:00 -0400

    GAME OFFICIALLY AND FINALLY DONE!!!!!

--- math_question_2.3.gd
+++ math_question_2.3.gd
@@ -1,7 +1,10 @@
 extends Node2D
 
-func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("level3.tscn.tscn")
+
 
 func _on_button_2_pressed() -> void:
 	get_tree().change_scene_to_file("deathscene2.2.tscn")
+
+
+func _on_buttongbdg_pressed() -> void:
+	get_tree().change_scene_to_file("level3.tscn")
