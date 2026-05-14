286287547c0deefc9f5e1e486d87f368840c6c67
Miguella297 <mikeivanov2010@gmail.com>
2026-05-13 20:31:10 -0400

    making area2ds and enemies for the second minigame

---
+++ area_2d_2b+4.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+@export var target_scene_path: String = "res://deathscene bonus 2.tscn"
+
+func _ready():
+	# Connect the signal (If not already connected in the editor)
+	connect("body_entered", _on_body_entered)
+	
+func _on_body_entered(body):
+	# Check if the body is in your player (assuming its a Kinematicbody2D or a Characterbody2D)
+	
+	if body.is_in_group("Player"):
+		call_deferred("_fix")
+
+func _fix():
+	get_tree().change_scene_to_file(target_scene_path)
